3a2ec0a1833ae0a20ee9a8bf1d3d6fb9cbe9101c
Adrian <achapa@adrianchapa.dev>
2022-08-29 12:39:15 -0500

    added useRef option as well

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from 'react'
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('')
   // 1st method using state
+  const nameInputRef = useRef()
   // 2nd method is using ref and reading from input when we need it
 
   const nameInputChangeHandler = (e) => {
@@ -15,12 +16,27 @@ const SimpleInput = (props) => {
     event.preventDefault()
 
     console.log(enteredName)
+    // refs are always object with a current property we can access which holds value assigned to the ref (pointer at input element)
+    // we can access .value because input elements in JS ..object always have a value property
+    const enteredValue = nameInputRef.current.value
+    console.log(enteredValue)
+
+    setEnteredName('') //reset the input field using state
+    // nameInputRef.current.value=''
+    // technically can do same using ref but not good practice b/c you are modifying the DOM directly
+    // React should be the only thing manipulating the DOM
   }
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
